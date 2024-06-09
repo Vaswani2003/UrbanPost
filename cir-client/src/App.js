0939626fc33561d4a8e2bba1d58550aa64827b97
Vinamra Vaswani <100992956+Vaswani2003@users.noreply.github.com>
2024-06-09 20:29:24 +0530

    Update App.js

--- cir-client/src/App.js
+++ cir-client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Landing from './pages/landing';
+import Myposts from './pages/myposts';
+import Createpost from './pages/createpost';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,9 +11,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {<Landing />} />
-        <Route path = '/login' element = {<Login />} />
+        <Route path = "/landing" element = {<Landing />} />
+        <Route path = '/' element = {<Login />} />
         <Route path = '/signup' element = {<Signup />} />
+        <Route path = '/myposts' element = {<Myposts />} />
+        <Route path = '/createpost' element = {<Createpost />} />
       </Routes>
       </BrowserRouter>
     </div>
